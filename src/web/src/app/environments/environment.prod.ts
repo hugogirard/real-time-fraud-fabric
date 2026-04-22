64831621f73548891b588b27884695d041db8ab5
Hugo Girard <hugo.girard@microsoft.com>
2026-04-22 13:45:06 -0400

    added prod config

---
+++ src/web/src/app/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    clientId: '__clientId__',
+    authority: '__authority__',
+    apiScopes: ['__apiScopes__'],
+    apiBaseUrl: '__apiBaseUrl__',
+    redirectUrl: '__redirectUrl__'
+}
+
